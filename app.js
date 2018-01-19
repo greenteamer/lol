a1cc7823587a89547d87c9dd756505b657706b46
user <greenteamer@bk.ru>
2018-01-19 03:15:25 +0300

    minions

--- app.js
+++ app.js
@@ -1,49 +1,19 @@
-// определим классы
-class Champion {
+// import {Champion} from './models'
 
-    constructor(props) {
-        console.log('props: ', props)
-        this.level = props.level || 1;
-        this.helth = this.level * 10;
-        this.force = this.level * 1;
-        this.name = props.name || 'unknown champ'
-        this.imageUrl = props.imageUrl || ''
-    }
-
-    punch(champ) {
-        champ.helth = champ.helth - this.force;
-        const isIAmWin = champ.isAlive()
-        if (!isIAmWin) {
-
-            // winner
-            var img = document.createElement("img");
-            img.src = this.imageUrl;
-            var src = document.getElementById("root");
-            src.appendChild(img);
-            
-            // result
-            console.log(this.name, ': Yahoo, I am win... gg, \n I have ', this.helth, ' HP');
-            console.log(champ.name, ': Oops, I am died..., \n I have ', champ.helth, ' HP');
-        }
-    }
-
-    isAlive() {
-        if (this.helth > 0) return true
-        return false
-    }
-
-}
-
-const kain = new Champion({level: 5, name: 'Kain', imageUrl: './kayn.jpg'});
-const hekarim = new Champion({level: 4, name: 'Hekarim', imageUrl: './heka.jpeg'});
-
+const kain = new Champion({level: 7, name: 'Kain', imageUrl: './kayn.jpg'});
+const hekarim = new Champion({level: 6, name: 'Hekarim', imageUrl: './heka.jpeg'});
+const minionRed = new Minion({level: 2, team: 'Minion red', imageUrl: './minionred.png'});
+const minionBlue = new Minion({level: 2, team:'Minion blue', imageUrl: './minionblue.jpg'})
 console.log('heka helth: ', hekarim.helth);
 console.log('kain helth: ', kain.helth);
 
 while (kain.isAlive() && hekarim.isAlive()) {
     console.log('took on youre head');
     hekarim.punch(kain);
+    minionRed.punch(kain)
     if (kain.isAlive()) {
         kain.punch(hekarim);
+        minionRed.punch(hekarim)
     }
 }
+
---
+++ models.js
@@ -0,0 +1,60 @@
+class Champion {
+
+  constructor(props) {
+      console.log('props: ', props)
+      this.level = props.level || 1;
+      this.helth = this.level * 10;
+      this.force = this.level * 1;
+      this.name = props.name || 'unknown champ'
+      this.imageUrl = props.imageUrl || ''
+  }
+
+  punch(champ) {
+      // champ.helth = champ.helth - this.force - minion.forse;
+      champ.helth = champ.helth - this.force
+
+      if (!champ.isAlive()) {
+
+          // winner
+          var img = document.createElement("img");
+          img.src = this.imageUrl;
+          var src = document.getElementById("root");
+          src.appendChild(img);
+          
+          // result
+          console.log(this.name, ': Yahoo, I am win... gg, \n I have ', this.helth, ' HP');
+          console.log(champ.name, ': Oops, I am loh..., \n I have ', champ.helth, ' HP');
+      }
+  }
+
+  isAlive() {
+      if (this.helth > 0) return true
+      return false
+  }
+
+}
+
+
+class Minion {
+ 
+  constructor(props) {
+    console.log('props', props)
+    this.level = props.level || 1
+    this.helth = this.level * 10
+    this.force = this.level * 0.5
+    this.team = props.team
+    this.imageUrl = props.imageUrl || ''
+  }
+  show() {
+    var img = document.createElement("img")
+    img.src = this.imageUrl
+    var src = document.getElementById("root")
+    src.appendChild(img);
+  }
+
+  punch(champ){
+    champ.helth = champ.helth - this.force;
+    console.log(champ.helth)
+  }
+}
+
