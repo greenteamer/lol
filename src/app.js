0de165b29a6634ed37b53e69e4297cf6a6199a81
Unknown <greenteamer@bk.ru>
2018-01-20 03:08:08 +0300

    Встречайте! Грини!

--- src/app.js
+++ src/app.js
@@ -11,14 +11,15 @@ const redTeam = new Team({team: RED_TEAM})
 const blueTeam = new Team({team: BLUE_TEAM})
 
 //Create champion
-const kain = new Champion({level: 7, name: 'Kain', imageUrl: './src/img/kayn.jpg' ,team: blueTeam});
-const hekarim = new Champion({level: 8, name: 'Hekarim', imageUrl: './src/img/heka.jpeg', team: redTeam});
-const dovabon = new Champion({level: 7, name: 'Dovabon', imageUrl: './src/img/dovabon.png', team: blueTeam});
+const kain = new Champion({level: 7, name: 'Kain', imageUrl: './src/img/kayn.jpg'});
+const hekarim = new Champion({level: 8, name: 'Hekarim', imageUrl: './src/img/heka.jpeg'});
+const dovabon = new Champion({level: 7, name: 'Dovabon', imageUrl: './src/img/dovabon.png'});
+const greene = new Champion({level: 8, nsme: 'Greene', imageUrl: './src/img/greene.jpg'})
 
 
 //Create minions
-const minionRed = new Minion({level: 2, team: 'Minion red', imageUrl: './src/img/minionred.png', team: redTeam});
-const minionBlue = new Minion({level: 2, team:'Minion blue', imageUrl: './src/img/minionblue.jpg', team: blueTeam})
+const minionRed = new Minion({level: 2, team: 'Minion red', imageUrl: './src/img/minionred.png'});
+const minionBlue = new Minion({level: 2, team:'Minion blue', imageUrl: './src/img/minionblue.jpg'})
 
 //Console
 console.log('heka helth: ', hekarim.helth);
